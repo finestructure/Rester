74e0a0c99fc646b1f2be4b1e7a86c3f684da07e7
Sven A. Schmidt <sas@finestructure.co>
2019-01-21 08:28:28 +0100

    Report results

--- Sources/Rester/main.swift
+++ Sources/Rester/main.swift
@@ -30,15 +30,17 @@ func wait(timeout: TimeInterval, until: () -> Bool) {
 }
 
 
-func launch(request: Request, named name: String) throws -> Promise<Void> {
+func launch(request: Request, named name: String) throws -> Promise<Bool> {
     print("→  \(name.blue) started ...")
     return try request.test()
         .map {
             switch $0 {
             case .valid:
                 print("✅  \(name.blue) \("PASSED".green.bold)")
+                return true
             case .invalid(let message):
                 print("❌  \(name.blue) \("FAILED".red.bold) : \(message.red)")
+                return false
             }
     }
 }
@@ -56,17 +58,31 @@ let main = command { (filename: String) in
             return
         }
 
+        var results = [Bool]()
         var chain = Promise()
+
+        // FIXME: run tests in order
         for name in requests.keys {
-            chain = chain.then { try launch(request: try rester.request(name), named: name) }
+            chain = chain.then {
+                try launch(request: try rester.request(name), named: name)
+                    .map {
+                        results.append($0)
+                }
+            }
         }
 
         var done = false
         _ = chain.done {
-            print("Done.")
             done = true
         }
         wait(timeout: 10) { done }
+
+        let failureCount = results.filter { !$0 }.count
+        let failureMsg = failureCount == 0 ? "0".green.bold : failureCount.description.red.bold
+        print("Executed \(results.count.description.bold) tests, with \(failureMsg) failures")
+        if failureCount > 0 {
+            exit(1)
+        }
 
     } catch {
         print("Error: \(error)")
